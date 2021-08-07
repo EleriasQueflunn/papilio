9b8311050fa9f902de92cfce3f6dd16da4e62008
EleriasQueflunn <70859590+EleriasQueflunn@users.noreply.github.com>
2021-08-07 16:44:27 +0200

    Add func.c to compile

--- make_w.bat
+++ make_w.bat
@@ -1,5 +1,5 @@
 mkdir obj
 cd obj
-gcc.exe -c -Wall -Ofast ../src/md2.c ../src/md45.c ../src/sha01.c ../src/sha2.c ../src/sha3.c ../src/hash.c ../src/papilio.c
+gcc.exe -c -Wall -Ofast ../src/md2.c ../src/md45.c ../src/sha01.c ../src/sha2.c ../src/sha3.c ../src/hash.c ../src/papilio.c ../src/func.c
 cd ..
-gcc.exe -Wall -Ofast obj/md2.o obj/md45.o obj/sha01.o obj/sha2.o obj/sha3.o obj/hash.o obj/papilio.o -o papilio
+gcc.exe -Wall -Ofast obj/md2.o obj/md45.o obj/sha01.o obj/sha2.o obj/sha3.o obj/hash.o obj/papilio.o obj/func.o -o papilio
